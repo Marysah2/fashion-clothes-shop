40f8ee11fd90f11e6a5a00b81a1c65c719090f9a
Sharon Soyian Lenjir <sharon.lenjir@student.moringaschool.com>
2026-02-11 15:25:29 +0300

    Resolve merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import ProductsPage from './Products/ProductsPage';
 import CartPage from './Components/CartPage';
 import CheckoutPage from './Checkout/CheckoutPage';
 import OrderSuccess from './Checkout/OrderSuccess';
+import OrderHistory from './features/orders/OrderHistory';
+import AdminOrders from './features/orders/AdminOrders';
+import AnalyticsDashboard from './features/admin/AnalyticsDashboard';
+import './index.css';
 
 export default function App() {
   return (
@@ -12,6 +16,10 @@ export default function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/order-success" element={<OrderSuccess />} />
+        <Route path="/orders/history" element={<OrderHistory />} />
+        <Route path="/admin/analytics" element={<AnalyticsDashboard />} />
+        <Route path="/admin/orders" element={<AdminOrders />} />
+        <Route path="*" element={<div>404</div>} />
       </Routes>
     </BrowserRouter>
   );
