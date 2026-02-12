38b5e1dd2d67321c2918bfcdea2f953010c25e58
Marysah <Marysah2@gmail.com>
2026-02-12 23:40:45 +0300

    initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", 
+  baseURL: "https://fashion-clothes-shop-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
