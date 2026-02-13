c28ac54d2228e4a1da62e6d44776a8059669efae
JosephNjorog <njorojoe11173@gmail.com>
2026-02-13 11:12:30 +0300

    Use Render API URL in frontend

--- src/slices/productsSlice.js
+++ src/slices/productsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export const fetchProducts = createAsyncThunk('products/fetchProducts', async (category) => {
   const url = category ? `${API_URL}/products?category=${category}` : `${API_URL}/products`;
